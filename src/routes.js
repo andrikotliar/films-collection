7c61487d4a971920536d93768ae52e153d99f02d
Andrii Kotliar <andrijkotliar@gmail.com>
2020-12-23 16:07:45 +0200

    added years page route

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,12 @@
-import Home from './components/Home.vue'
-import Film from './components/Film.vue'
-import List from './components/List.vue'
-import Category from './components/Category.vue'
-import AllFilms from './components/AllFilms.vue'
-import Trailers from './components/Trailers.vue'
-import Twitter from './components/Twitter.vue'
+import Home from './components/Home.vue';
+import Film from './components/Film.vue';
+import List from './components/List.vue';
+import Category from './components/Category.vue';
+import AllFilms from './components/AllFilms.vue';
+import Trailers from './components/Trailers.vue';
+import Twitter from './components/Twitter.vue';
+import YearsList from './components/YearsList.vue';
+import Year from './components/Year.vue';
 
 const routes = [
 	{path: '/', component: Home},
@@ -29,9 +31,9 @@ const routes = [
 		component: Category
 	},
 	{
-		path: '/year/:category', 
+		path: '/years/:year', 
 		props: true,
-		component: Category
+		component: Year
 	},
 	{
 		path: '/search/:category', 
@@ -44,7 +46,8 @@ const routes = [
 		component: AllFilms
 	},
 	{path: '/trailers', component: Trailers},
-	{path: '/twitter', component: Twitter}
+	{path: '/twitter', component: Twitter},
+	{path: '/years', component: YearsList}
 ]
 
 export default routes
