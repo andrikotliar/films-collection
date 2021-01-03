87c23cc52c008bbfbb2ffaf2e3e31dc9aad43da4
Andrii Kotliar <andrijkotliar@gmail.com>
2021-01-03 11:23:17 +0200

    new rows

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,25 +1,38 @@
 <template>
 	<div class="page container">
 		<Search />
-		<FilmsRow category="2019" title="The newest films" reverse="true"/>
-		<FilmsRow category="c-fav" title="The most favorite films" reverse="false"/>
-		<FilmsRow category="c-mw" title="The most watched films" reverse="true"/>
-		<FilmsRow category="c-cw" title="Cinema watched films" reverse="true"/>
-		<FilmsRow category="c-tv" title="TV Series" reverse="true" className="elems-3" />
-		<FilmsRow category="c-marvel" title="Marvel Cinematic Universe" reverse="true"/>
+		<FilmsRow property="year" :category="previousYear" title="The newest films" link="years" reverse />
+		<AllFilms />
+		<FilmsRow property="categories" category="special-favorite" title="The most favorite films" />
+		<FilmsRow property="categories" category="most-watched" title="The most watched films" reverse />
+		<FilmsRow property="categories" category="cinema-watched" title="Cinema watched films" reverse />
+		<FilmsRow property="categories" category="series" title="TV Series" reverse />
+		<FilmsRow property="categories" category="marvel" title="Marvel Cinematic Universe" reverse />
 		<FilmsRowYears />
 	</div>
 </template>
 
 <script>
 	import Search from 'elements/Search.vue';
-	import FilmsRow from 'elements/Categories.vue';
+	import FilmsRow from 'elements/Category.vue';
+	import AllFilms from 'elements/AllFilmsCategory.vue';
 	import FilmsRowYears from 'elements/Years.vue';
 	export default {
 		name: 'Home',
+		data() {
+			return {
+				currentYear: new Date().getFullYear()
+			}
+		},
+		computed: {
+			previousYear() {
+				return this.currentYear - 1;
+			}
+		},
 		components: {
 			Search,
 			FilmsRow,
+			AllFilms,
 			FilmsRowYears
 		}
 	}
