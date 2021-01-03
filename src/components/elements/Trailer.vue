0ef419ec66754ab6b1a4640506114489df1f93e8
Andrii Kotliar <andrijkotliar@gmail.com>
2021-01-03 10:37:19 +0200

    changed class names

--- src/components/elements/Trailer.vue
+++ src/components/elements/Trailer.vue
@@ -1,10 +1,10 @@
 <template>
-	<div class="film-info__trailer" @click="addTrailer">
-		<img :src="`https://i.ytimg.com/vi/${youtube}/hqdefault.jpg`" class="film-info__trailer-cover" alt="Avatar Trailer">
-		<button class="film-info__trailer-btn">
+	<div class="film__trailer" @click="addTrailer">
+		<img :src="`https://i.ytimg.com/vi/${youtube}/hqdefault.jpg`" class="film__trailer-cover" alt="">
+		<button class="film__trailer-btn" @click="addTrailer">
 			<img src="images/icons/play.svg" alt="Play button">
 		</button>
-	</div>
+	</div>		
 </template>
 
 <script>
@@ -13,15 +13,15 @@
 		props: ['youtube'],
 		methods: {
 			addTrailer() {
-				let trailer = document.querySelector('.film-info__trailer');
-				let button = trailer.querySelector('.film-info__trailer-btn');
-				let cover = trailer.querySelector('.film-info__trailer-cover');
+				let trailer = document.querySelector('.film__trailer');
+				let button = trailer.querySelector('.film__trailer-btn');
+				let cover = trailer.querySelector('.film__trailer-cover');
 				let iframe = document.createElement('iframe');
 
 				iframe.setAttribute('allowfullscreen', '');
 			    iframe.setAttribute('allow', 'autoplay');
 			    iframe.setAttribute('src', `https://www.youtube.com/embed/${this.youtube}?rel=0&showinfo=0&autoplay=1`);
-			    iframe.classList.add('film-info__trailer-cover');	
+			    iframe.classList.add('film__trailer-cover');	
 
 		        button.remove();
 		        cover.remove();
