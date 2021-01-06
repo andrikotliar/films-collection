c83bd63485a39c8745ce3c07e9b46af192cea585
Andrii Kotliar <andrijkotliar@gmail.com>
2021-01-06 10:06:59 +0200

    added preloader component

---
+++ src/components/elements/Preloader.vue
@@ -0,0 +1,35 @@
+<template>
+	<div class="preloader" :style="`top: ${position}px`" v-if="!loaded">
+		<div class="container">
+			<img src="images/icons/preloader.svg" alt="">			
+		</div>
+	</div>
+</template>
+
+<script>
+	export default {
+		name: 'Preloader',
+		props: ['position'],
+		data() {
+			return {
+				loaded: false
+			}
+		},
+		methods: {
+			hideLoader() {
+				this.loaded = true;
+			}
+		},
+		watch: {
+			'$route'() {
+				this.loaded = false;
+				setTimeout(this.hideLoader, 1000);
+			}
+		},
+		mounted() {
+			setTimeout(this.hideLoader, 1000);
+		}
+	}
+</script>
+
+<style src="stylesElems/preloader.css"></style>
