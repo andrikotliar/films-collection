2b76a9dc73423c1bdca712acb9c804d37a01799b
Andrii Kotliar <andrikotliar@gmail.com>
2021-01-16 10:53:44 +0200

    add gulp merge json files module

---
+++ gulpfile.js
@@ -0,0 +1,13 @@
+const gulp = require('gulp');
+const merge = require('./src/modules/merge.js');
+
+const mergeJSON = () => {
+	return gulp.src('json/*.json')
+	.pipe(merge('database.json', (data) => {
+		let buffer = Buffer.from(JSON.stringify(data));
+		return buffer;
+	}))
+	.pipe(gulp.dest('database'));	
+}
+
+exports.mergeJSON = mergeJSON;
