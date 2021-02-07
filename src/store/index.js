a4c43f5edc6b9e99c6cbe0003937ef00254c8e0d
Andrii Kotliar <andrikotliar@gmail.com>
2021-02-07 11:55:26 +0200

    add calender data fetch

--- src/store/index.js
+++ src/store/index.js
@@ -5,20 +5,31 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
     state: {
-        films: []
+        films: [],
+        calender: []
     },
     actions: {
         LOAD_FILMS ({ commit }) {
           fetch('database/database.json')
             .then(response => response.json())
             .then(films => {
-            commit('SET_FILMS', films)
+                commit('SET_FILMS', films)
             })          
+        },
+        LOAD_CALENDER ({ commit }) {
+            fetch('https://spreadsheets.google.com/feeds/list/1ByXhpC1xM8lOQNEB3xc3sb3uwlJrLgy7zA4PUOx52cg/od6/public/values?alt=json')
+                .then(response => response.json())
+                .then(calender => {
+                    commit('SET_CALENDER', calender.feed.entry)
+                });
         }
     },
     mutations: {
         SET_FILMS(state, films) {
             state.films = films
+        },
+        SET_CALENDER(state, calender) {
+            state.calender = calender;
         }
     }
 })
