3abe3c57a19c4c23c8ab5fee3c6d61567e19b920
Andrii Kotliar <andrikotliar@gmail.com>
2021-03-15 09:13:09 +0200

    initial YearsSelect component

--- src/components/categories/YearsCategory.vue
+++ src/components/categories/YearsCategory.vue
@@ -1,73 +1,105 @@
 <template>
 	<div class="category">
 		<div class="category__header">
-			<h2 class="category__title"> Films by years </h2>
+			<h2 class="category__title"> Films by year </h2>
 		</div>
-		<div class="category__body category__body--byYear">
-			<router-link :to="`/years/${year}`" class="category__year" v-for="(year, index) in years" :key="index">
+		<div class="category__years">
+			<router-link :to="`/year/${year}`" class="category__year" v-for="(year, index) in years.slice(0, yearsToShow)" :key="index">
 				{{year}}
 			</router-link>
-			<router-link to="/years/" class="category__year category__year--more">
-				More
-			</router-link>
 		</div>
+		<YearsSelect disabledText="Select early years" :yearsToShow="yearsToShow" className="select-wrapper--homeYears" />
 	</div>
+
 </template>
 
 <script>
+	import YearsSelect from './../parts/YearsSelect';
 	export default {
 		name: 'YearsCategory',
+		data() {
+			return {
+				yearsToShow: 12
+			}
+		},
 		computed: {
 			years() {
 				let currentYear = new Date().getFullYear();
 				let yearsList = []
-				for(let i = 0; i < 11; i++) {
+				for(let i = 0; i < 12; i++) {
 					currentYear--;
 					yearsList.push(currentYear);
 				}
 				return yearsList;
 			}
+		},
+		components: {
+			YearsSelect
 		}
 	}
 </script>
 
 <style>
-	.category__body--byYear {
-		flex-wrap: wrap;
-		gap: 10px;
+	.category__years {
+		--columns: 6;
+		display: grid;
+		grid-template-columns: repeat(var(--columns), 1fr);
+		grid-gap: 10px;
+	}
+	@media (max-width: 1300px) {
+		.category__years {
+			--columns: 4;
+		}
+	}
+	@media (max-width: 720px) {
+		.category__years {
+			--columns: 3;
+		}
 	}
 	.category__year {
-		color: var(--base-color);
+		position: relative;
+		color: #fff;
 		font-weight: bold;
 		padding: 10px;
-		width: 160px;
 		text-align: center;
-		border: 4px solid var(--base-color);
+		background-color: var(--base-color);
 		transition: .3s;
 	}
-	@media (max-width: 750px) {
-		.category__year {
-			width: 200px;
-		}
+	.category__year:before,
+	.category__year:after {
+		position: absolute;
+		width: 10px;
+		height: 10px;
+		transition: .3s;
+		content: '';		
 	}
-	@media (max-width: 700px) {
-		.category__year {
-			width: calc(50% - 10px);
-		}
+	.category__year:before {
+		bottom: -2px;
+		right: -2px;
+		border-right: 2px solid #fff;
+		border-bottom: 2px solid #fff;
 	}
-	.category__year:hover {
-		color: #fff;
-		background-color: var(--base-color);
+	.category__year:after {
+		top: -2px;
+		left: -2px;
+		border-left: 2px solid #fff;
+		border-top: 2px solid #fff;
 	}
-
-	.category__year--more {
-		color: #000;
-		background-color: #eee;
-		border-color: #eee;
+	.category__year:hover:before,
+	.category__year:hover:after {
+		width: calc(100% - 10px);
+		height: calc(100% - 10px);		
 	}
-	.category__year--more:hover {
-		color: #000;
-		background-color: #ddd;
-		border-color: #ddd;
+	.category__year:hover:before { 
+		bottom: 5px;
+		right: 5px;		
+	}
+	.category__year:hover:after {
+		top: 5px;
+		left: 5px;		
+	}
+	.select-wrapper--homeYears {
+		margin-top: 20px;
+		width: min(400px, 100%);
 	}
 </style>
