7b5e662a9717ae7aa9b4cae758067e38554fea1f
Andrii Kotliar <andrii.ktlr@gmail.com>
2023-06-04 23:13:09 +0300

    removed unused export

--- src/types/index.ts
+++ src/types/index.ts
@@ -2,5 +2,4 @@ export * from './actor-type';
 export * from './filled-actor-data-type';
 export * from './film-type';
 export * from './filter-type';
-export * from './schema-type';
 export * from './unknown-object-type';
