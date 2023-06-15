d055af73dfbec8f1837a349706ebb7d0f120d367
Andrii Kotliar <andrii.ktlr@gmail.com>
2023-06-15 19:58:41 +0300

    added UnknownObject type to the get-filter-params

--- src/heplers/get-filter-params.ts
+++ src/heplers/get-filter-params.ts
@@ -1,5 +1,7 @@
+import { UnknownObject } from "@/types";
+
 export const getFilterParams = (searchParams: URLSearchParams) => {
-  const filterParams = {};
+  const filterParams: UnknownObject = {};
   searchParams.forEach((value, key) => {
     if(filterParams[key]) {
       filterParams[key] = [filterParams[key], value];
