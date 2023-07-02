243bc0afcfc4c9b8e06b011e384321ef297ebfb9
Andrii Kotliar <andrii.ktlr@gmail.com>
2023-07-02 22:15:02 +0300

    added custom button component

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,10 +2,12 @@ import Header from '@/components/Header';
 import Search from '@/components/Search';
 import Loader from '@/components/Loader';
 import ResizableButton from '@/components/ResizableButton';
+import Button from '@/components/Button';
 
 export {
   Header,
   Search,
   Loader,
-  ResizableButton
+  ResizableButton,
+  Button
 };
