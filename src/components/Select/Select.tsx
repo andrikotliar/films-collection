21fb9217a8c757f38b006107362a40ccda943700
Andrii Kotliar <andrii.ktlr@gmail.com>
2023-10-08 18:02:00 +0300

    Add more keyboard interaction in Select and recognizing default value

--- src/components/Select/Select.tsx
+++ src/components/Select/Select.tsx
@@ -1,6 +1,6 @@
 import classNames from 'classnames';
 import classes from './Select.module.css';
-import { FC, KeyboardEvent, useState } from 'react';
+import { FC, KeyboardEvent, useEffect, useState } from 'react';
 import { ExpandIcon } from '@/assets/icons';
  
 type Option = {
@@ -10,7 +10,7 @@ type Option = {
 
 type SelectProps = {
   options: Option[];
-  defaultValue?: Option;
+  defaultValue?: Option['value'];
   onSelect?: (option: Option) => void;
 };
 
@@ -19,35 +19,76 @@ const Select: FC<SelectProps> = ({
   defaultValue,
   onSelect,
 }) => {
-  const [selectedOption, setSelectedOption] = useState(defaultValue || options[0]);
+  const [selectedOption, setSelectedOption] = useState(options[0]);
+  const [activeIndex, setActiveIndex] = useState(0);
   const [isOpen, setIsOpen] = useState(false);
 
-  const handleChange = (option: Option) => {
+  const selectOption = (option: Option) => {
+    setSelectedOption(option);
+    onSelect && onSelect(option);
+  }
+
+  const handleChange = (option: Option, index: number) => {
+    selectOption(option)
+    setActiveIndex(index);
     setIsOpen(false);
-    if(selectedOption.value !== option.value) {
-      setSelectedOption(option);
-      onSelect && onSelect(option);
-    }
   };
 
   const handleDropdown = () => {
     setIsOpen(isOpen => !isOpen);
   }
 
-  const handleDropdownOnKey = (e: KeyboardEvent<HTMLDivElement>) => {
-    if(e.key === 'Enter') {
-      handleDropdown();
-    }
+  const handleDropdownOnKey = (
+    e: KeyboardEvent<HTMLDivElement>
+  ) => {
+    switch(e.key) {
+      case ' ':
+        e.preventDefault();
+        handleDropdown();
+        selectOption(options[activeIndex]);
+        break;
+      case 'Enter':
+        handleDropdown();
+        selectOption(options[activeIndex]);
+        break;
+      case 'Escape':
+        setIsOpen(false);
+        break;
+      case 'ArrowDown':
+        e.preventDefault();
+        setActiveIndex(index => {
+          if(index >= 0 && index < options.length - 1) {
+            return index + 1;
+          }
 
-    if(e.key === 'Escape') {
-      setIsOpen(false);
-    }
+          return index;
+        });
+        break;
+      case 'ArrowUp':
+        e.preventDefault();
+        setActiveIndex(index => {
+          if(index > 0) {
+            return index - 1;
+          }
 
-    if(e.key === ' ') {
-      e.preventDefault();
-      setIsOpen(true);
+          return index;
+        });
+        break;
+      default:
+        break;
     }
   }
+
+  useEffect(() => {
+    if(defaultValue) {
+      const index = options.findIndex((option) => option.value === defaultValue);
+
+      if(index !== -1) {
+        setSelectedOption(options[index]);
+        setActiveIndex(index);
+      }
+    }
+  }, [defaultValue]);
 
   return (
     <div
@@ -60,7 +101,7 @@ const Select: FC<SelectProps> = ({
       aria-expanded={isOpen}
     >
       <div className={classes.value}>
-        <span>{selectedOption?.label}</span>
+        <span>{selectedOption.label}</span>
         <ExpandIcon
           className={classNames(
             classes.expandIcon,
@@ -73,23 +114,25 @@ const Select: FC<SelectProps> = ({
       <ul
         className={classNames(
           classes.dropdown,
+          'custom-scroll',
           {
             [classes.visible]: isOpen
           }
         )}
         role="menu"
       >
-        {options.map((option) => (
+        {options.map((option, index) => (
           <li
             key={option.value}
             onClick={(e) => {
               e.stopPropagation();
-              handleChange(option);
+              handleChange(option, index);
             }}
             className={classNames(
               classes.option,
               {
-                [classes.selected]: option.value === selectedOption?.value
+                [classes.selected]: option.value === selectedOption.value,
+                [classes.highlighted]: activeIndex === index,
               }
             )}
             role="menuitem"
