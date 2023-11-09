55e35a9b35cac582bb234a938a23a71047aafb85
Andrii Kotliar <andrii.ktlr@gmail.com>
2023-11-09 09:53:32 +0200

    Remove select component, display diagram 100% width

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,7 +2,6 @@ export * from './Button';
 export * from './Header';
 export * from './Loader';
 export * from './Search';
-export * from './Select';
 export * from './Container';
 export * from './AppWrapper';
 export * from './FormCheckbox';
