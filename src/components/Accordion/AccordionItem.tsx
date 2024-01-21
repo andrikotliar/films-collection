78e8f7388221f22d090968def17f3c5a38b50371
Andrii Kotliar <andrii.ktlr@gmail.com>
2024-01-21 14:23:13 +0200

    add accordion basic styles

--- src/components/Accordion/AccordionItem.tsx
+++ src/components/Accordion/AccordionItem.tsx
@@ -1,32 +1,58 @@
+import { ExpandIcon } from '@/assets/icons';
 import { useAccordionContext } from '@/components/Accordion/accordion.context';
 import classNames from 'classnames';
 import { FC, PropsWithChildren } from 'react';
+import classes from './AccordionItem.module.css';
+
+type ClassNames = {
+  wrapper: string;
+  button: string;
+  body: string;
+  title: string;
+};
 
 type AccordionItemProps = {
   index: number;
   title?: string;
-  className?: string;
+  styles?: Partial<ClassNames>;
 };
 
 const AccordionItem: FC<PropsWithChildren<AccordionItemProps>> = ({
   children,
   index,
   title,
-  className,
+  styles,
 }) => {
   const { activeIndex, setActiveIndex } = useAccordionContext();
   const isOpen = activeIndex === index;
 
+  const handleActiveIndex = (index: number) => {
+    if (index === activeIndex) {
+      setActiveIndex(undefined);
+      return;
+    }
+
+    setActiveIndex(index);
+  };
+
   return (
-    <div className={className}>
-      {title && <button onClick={() => setActiveIndex(index)}>{title}</button>}
-      <div
-        className={classNames({
-          open: isOpen,
-        })}
-      >
-        {isOpen && <div>{children}</div>}
-      </div>
+    <div className={styles?.wrapper}>
+      {title && (
+        <button
+          className={classNames(classes.button, styles?.button)}
+          onClick={() => handleActiveIndex(index)}
+        >
+          <span className={styles?.title}>{title}</span>
+          <ExpandIcon
+            className={classNames(classes.expandIcon, {
+              [classes.expandIconOpen]: isOpen,
+            })}
+          />
+        </button>
+      )}
+      {isOpen && (
+        <div className={classNames(classes.body, styles?.body)}>{children}</div>
+      )}
     </div>
   );
 };
