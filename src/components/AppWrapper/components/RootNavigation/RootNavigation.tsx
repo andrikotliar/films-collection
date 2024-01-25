7d6fdcbea8e64d788e3e3d303c6e079df8698a5a
Andrii Kotliar <andrii.ktlr@gmail.com>
2024-01-25 10:48:41 +0200

    add basic mobile menu

--- src/components/AppWrapper/components/RootNavigation/RootNavigation.tsx
+++ src/components/AppWrapper/components/RootNavigation/RootNavigation.tsx
@@ -1,4 +1,3 @@
-import classNames from 'classnames';
 import classes from './RootNavigation.module.css';
 import { Scrollable } from '@/components/Scrollable';
 import { Link, useLocation } from 'react-router-dom';
@@ -6,13 +5,21 @@ import { FilmsCollectionLogo } from '@/assets/logos';
 import { Search } from '@/components/Search';
 import { Navigation } from '@/components/Navigation';
 import { globalMenu } from '@/configs';
+import { Menu } from 'lucide-react';
+import classNames from 'classnames';
+import { useEffect, useState } from 'react';
 
 const RootNavigation = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
   const location = useLocation();
 
   const checkIsActive = (currentLink: string) => {
     return location.pathname === currentLink;
   };
+
+  useEffect(() => {
+    setIsMenuOpen(false);
+  }, [location]);
 
   return (
     <Scrollable className={classes.navigation}>
@@ -28,7 +35,19 @@ const RootNavigation = () => {
         </Link>
         <Search />
       </div>
-      <Navigation links={globalMenu} checkIsActive={checkIsActive} />
+      <div
+        className={classNames(classes.menuWrapper, {
+          [classes.isOpen]: isMenuOpen,
+        })}
+      >
+        <Navigation links={globalMenu} checkIsActive={checkIsActive} />
+      </div>
+      <button
+        className={classes.mobileMenuButton}
+        onClick={() => setIsMenuOpen(true)}
+      >
+        <Menu />
+      </button>
     </Scrollable>
   );
 };
