e54af550bd6e8ec9328aaf1821cdd6e895b6c04f
Andrii Kotliar <andrii.ktlr@gmail.com>
2024-03-10 20:00:57 +0200

    update tabs design

--- src/components/Tabs/Tabs.tsx
+++ src/components/Tabs/Tabs.tsx
@@ -2,6 +2,7 @@ import { Scrollable } from '@/components/Scrollable';
 import styles from './Tabs.module.css';
 import { FC, ReactNode, useEffect, useState } from 'react';
 import classNames from 'classnames';
+import { ArrowLeft, ChevronLeft, ChevronRight } from 'lucide-react';
 
 type Props = {
   components: {
@@ -13,26 +14,89 @@ type Props = {
 
 const Tabs: FC<Props> = ({ components, defaultTabIndex = 0 }) => {
   const [activeIndex, setActiveIndex] = useState(defaultTabIndex);
+  const [isListOpen, setIsListOpen] = useState(false);
 
   useEffect(() => {
     setActiveIndex(defaultTabIndex);
   }, [defaultTabIndex]);
 
+  const handleListOpen = () => {
+    setIsListOpen((isOpen) => !isOpen);
+  };
+
+  const handlePrevGroup = () => {
+    setActiveIndex((prevIndex) => {
+      const nextIndex = prevIndex - 1;
+
+      if (nextIndex < 0) {
+        return components.length - 1;
+      }
+
+      return nextIndex;
+    });
+  };
+
+  const handleNextGroup = () => {
+    setActiveIndex((prevIndex) => {
+      const nextIndex = prevIndex + 1;
+
+      if (nextIndex === components.length) {
+        return 0;
+      }
+
+      return nextIndex;
+    });
+  };
+
+  const handleGroupSelect = (index: number) => () => {
+    setActiveIndex(index);
+    setIsListOpen(false);
+  };
+
   return (
     <div className={styles.tabs}>
       <div className={styles.controls}>
-        {components.map((component, index) => (
-          <button
-            type="button"
-            onClick={() => setActiveIndex(index)}
-            key={component.label}
-            className={classNames(styles.tab, {
-              [styles.active]: activeIndex === index,
-            })}
-          >
-            {component.label}
-          </button>
-        ))}
+        <button
+          type="button"
+          className={classNames(styles.arrow, styles.arrowLeft)}
+          onClick={handlePrevGroup}
+        >
+          <ChevronLeft />
+        </button>
+        <button
+          type="button"
+          onClick={handleListOpen}
+          className={styles.listControl}
+        >
+          {isListOpen ? (
+            <span className={styles.placeholder}>Select group</span>
+          ) : (
+            components[activeIndex].label
+          )}
+        </button>
+        <button
+          type="button"
+          className={classNames(styles.arrow, styles.arrowRight)}
+          onClick={handleNextGroup}
+        >
+          <ChevronRight />
+        </button>
+        {isListOpen && (
+          <div className={styles.groups}>
+            {components.map((component, index) => (
+              <button
+                type="button"
+                key={component.label}
+                className={classNames(styles.groupLabel, {
+                  [styles.selectedGroup]: index === activeIndex,
+                })}
+                onClick={handleGroupSelect(index)}
+              >
+                {component.label}
+              </button>
+            ))}
+          </div>
+        )}
       </div>
       <Scrollable className={styles.content}>
         {components[activeIndex].content}
