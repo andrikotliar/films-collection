d28d5bec053aeeca809c167d3d9d9e4175160248
Andrii Kotliar <andrii.ktlr@gmail.com>
2024-07-21 19:08:44 +0300

    increase movies per page

--- src/common/constants/index.ts
+++ src/common/constants/index.ts
@@ -1,6 +1,6 @@
 const APP_TITLE = 'Films Collection';
 
-const PER_PAGE = 36;
+const PER_PAGE = 48;
 const MOBILE_VIEW_BREAKPOINT_PX = 785;
 
 export { APP_TITLE, PER_PAGE, MOBILE_VIEW_BREAKPOINT_PX };
