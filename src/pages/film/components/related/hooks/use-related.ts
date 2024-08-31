a18de235906f8b6d7224731e3f6196f2fbcaa0cc
Andrii Kotliar <andrii.ktlr@gmail.com>
2024-08-31 18:02:17 +0300

    update related section content

--- src/pages/film/components/related/hooks/use-related.ts
+++ src/pages/film/components/related/hooks/use-related.ts
@@ -1,53 +1,35 @@
 import { RelatedFilms, RelatedItem } from '@/common/types';
-import { splitChapters } from '@/pages/film/components/related/helpers';
 import { useMemo } from 'react';
 
-enum ChaptersTab {
-  PREQUELS = 'prequels',
-  SEQUELS = 'sequels',
+enum RelatedTitlesTab {
+  CHAPTERS = 'sequels',
   REMAKES = 'remakes',
   ORIGINALS = 'originals',
 }
 
 type Tabs = {
-  id: ChaptersTab;
+  id: RelatedTitlesTab;
   name: string;
   content: RelatedItem[];
+  isNumerationShown?: boolean;
 };
 
 const useRelated = (related: RelatedFilms, currentFilmId: string): Tabs[] => {
   const data = useMemo(() => {
-    const chaptersIndex = related.chapters.findIndex(
-      (data) => data.id === currentFilmId,
-    );
-
-    const chaptersFilledWithNumbers = related.chapters.map(
-      (chapter, index) => ({
-        ...chapter,
-        chapter: index + 1,
-      }),
-    );
-
-    const chapters = splitChapters(chaptersIndex, chaptersFilledWithNumbers);
-
     const values: Tabs[] = [
       {
-        id: ChaptersTab.PREQUELS,
-        name: 'Prequels',
-        content: chapters.prequels,
+        id: RelatedTitlesTab.CHAPTERS,
+        name: 'Chapters',
+        content: related.chapters ?? [],
+        isNumerationShown: true,
       },
       {
-        id: ChaptersTab.SEQUELS,
-        name: 'Sequels',
-        content: chapters.sequels,
-      },
-      {
-        id: ChaptersTab.REMAKES,
+        id: RelatedTitlesTab.REMAKES,
         name: 'Remakes',
         content: related.remakes ?? [],
       },
       {
-        id: ChaptersTab.ORIGINALS,
+        id: RelatedTitlesTab.ORIGINALS,
         name: 'Originals',
         content: related.originals ?? [],
       },
@@ -59,4 +41,4 @@ const useRelated = (related: RelatedFilms, currentFilmId: string): Tabs[] => {
   return data;
 };
 
-export { useRelated, ChaptersTab };
+export { useRelated, RelatedTitlesTab };
