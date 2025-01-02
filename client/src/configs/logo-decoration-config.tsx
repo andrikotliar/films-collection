999b2cc1d46431b2ea3d51b8aa5965df6f03f83a
Andrii Kotliar <andrii.ktlr@outlook.com>
2025-01-02 11:08:34 +0200

    update logo decoration config

--- client/src/configs/logo-decoration-config.tsx
+++ client/src/configs/logo-decoration-config.tsx
@@ -1,44 +1,53 @@
-export const getLogoDecorationConfig = (year: number) => {
-  return [
-    {
-      title: "Author's Birthday",
-      image: 'decoration/birthday.png',
-      date: {
-        from: `${year}-10-09`,
-        to: `${year}-10-10`,
-      },
-    },
-    {
-      title: 'World Cinema Day',
-      image: 'decoration/cinema.png',
-      date: {
-        from: `${year}-12-28`,
-        to: `${year}-12-28`,
-      },
-    },
-    {
-      title: 'International Harry Potter Day',
-      image: 'decoration/hogwarts.png',
-      date: {
-        from: `${year}-05-02`,
-        to: `${year}-05-02`,
-      },
-    },
-    {
-      title: 'Star Wars Day',
-      image: 'decoration/darth_vader.png',
-      date: {
-        from: `${year}-05-04`,
-        to: `${year}-05-04`,
-      },
-    },
-    {
-      title: 'Christmas Time',
-      image: 'decoration/christmas_tree.png',
-      date: {
-        from: `${year}-12-06`,
-        to: `${year + 1}-01-02`,
-      },
-    },
-  ];
+export type LogoDecorationDate = [number, number];
+
+export type LogoDecorationItem = {
+  title: string;
+  image: string;
+  date: {
+    from: LogoDecorationDate;
+    to: LogoDecorationDate;
+  };
 };
+
+export const logoDecorationConfig: LogoDecorationItem[] = [
+  {
+    title: "Author's Birthday",
+    image: 'decoration/birthday.png',
+    date: {
+      from: [10, 9],
+      to: [10, 10],
+    },
+  },
+  {
+    title: 'World Cinema Day',
+    image: 'decoration/cinema.png',
+    date: {
+      from: [12, 28],
+      to: [12, 28],
+    },
+  },
+  {
+    title: 'International Harry Potter Day',
+    image: 'decoration/hogwarts.png',
+    date: {
+      from: [5, 2],
+      to: [5, 2],
+    },
+  },
+  {
+    title: 'Star Wars Day',
+    image: 'decoration/darth_vader.png',
+    date: {
+      from: [5, 4],
+      to: [5, 4],
+    },
+  },
+  {
+    title: 'Christmas Time',
+    image: 'decoration/christmas_tree.png',
+    date: {
+      from: [12, 6],
+      to: [1, 15],
+    },
+  },
+];
--- client/src/hooks/use-logo-decoration.ts
+++ client/src/hooks/use-logo-decoration.ts
@@ -1,26 +1,53 @@
-import { getLogoDecorationConfig } from '@/configs';
-import { buildMediaPath } from '@/helpers';
 import { useMemo } from 'react';
+import { logoDecorationConfig, LogoDecorationDate } from '@/configs';
+import { buildMediaPath } from '@/helpers';
 
 type DecorationEvent = {
   image: string;
   title: string;
 };
 
+const convertDateToNumber = ([month, day]: LogoDecorationDate) => {
+  return month * 100 + day;
+};
+
+const getIsDateBetween = (
+  currentDate: number,
+  startDate: number,
+  endDate: number,
+) => {
+  if (startDate <= endDate) {
+    return currentDate >= startDate && currentDate <= endDate;
+  }
+
+  return currentDate >= startDate || currentDate <= endDate;
+};
+
 export const useLogoDecoration = () => {
   return useMemo(() => {
-    const currentDate = new Date();
-    const currentYear = currentDate.getFullYear();
+    const now = new Date();
+    const date = now.getDate();
+    const month = now.getMonth() + 1;
 
-    const events = getLogoDecorationConfig(currentYear);
+    const events = logoDecorationConfig.filter((event) => {
+      return event.date.from[0] === month || event.date.to[0] === month;
+    });
+
+    if (!events.length) {
+      return null;
+    }
 
     let decorationEvent: DecorationEvent | null = null;
 
-    for (const event of events) {
-      const fromDate = new Date(event.date.from);
-      const toDate = new Date(event.date.to);
+    const currentDate = convertDateToNumber([month, date]);
 
-      if (currentDate >= fromDate && currentDate <= toDate) {
+    for (const event of events) {
+      const startDate = convertDateToNumber(event.date.from);
+      const endDate = convertDateToNumber(event.date.to);
+
+      const isDateBetween = getIsDateBetween(currentDate, startDate, endDate);
+
+      if (isDateBetween) {
         decorationEvent = {
           image: buildMediaPath(event.image),
           title: event.title,
