d01ead9ab3447b12ae50f78c9165fd9c687a1059
Andrii Kotliar <andrii.ktlr@outlook.com>
2025-03-12 20:43:52 +0200

    add trailers preview track to the trailers modal for the series type

--- client/src/pages/film/components/SummarySection/components/Trailers/components/TrailersModal/TrailersModal.tsx
+++ client/src/pages/film/components/SummarySection/components/Trailers/components/TrailersModal/TrailersModal.tsx
@@ -1,23 +1,26 @@
 import { Modal } from '@/ui';
 import { FC } from 'react';
-import { Video } from '../Video/Video';
 import styles from './TrailersModal.module.css';
+import { Season } from '@/types';
+import { Trailers } from './components';
 
 type TrailersModalProps = {
-  isOpen: boolean;
   onClose: VoidFunction;
-  trailerId: string;
+  isOpen: boolean;
+  trailerId: string | null;
+  seasons: Season[];
 };
 
 export const TrailersModal: FC<TrailersModalProps> = ({
-  isOpen,
   onClose,
+  isOpen,
   trailerId,
+  seasons,
 }) => {
   return (
     <Modal isOpen={isOpen} onClose={onClose} className={styles.trailersModal}>
       <Modal.Content className={styles.trailersModalContent}>
-        <Video trailerId={trailerId} />
+        <Trailers trailerId={trailerId} seasons={seasons} />
         <Modal.CloseButton
           onClick={onClose}
           className={styles.trailerCloseButton}
---
+++ client/src/pages/film/components/SummarySection/components/Trailers/components/TrailersModal/components/Video/Video.tsx
@@ -0,0 +1,21 @@
+import { FC } from 'react';
+import styles from './Video.module.css';
+
+type VideoProps = {
+  trailerId: string;
+  shouldAutoPlay: boolean;
+};
+
+export const Video: FC<VideoProps> = ({ trailerId, shouldAutoPlay }) => {
+  const autoPlayOption = shouldAutoPlay ? '1' : '0';
+
+  return (
+    <div className={styles.video}>
+      <iframe
+        src={`https://www.youtube-nocookie.com/embed/${trailerId}?rel=0&showinfo=0&autoplay=${autoPlayOption}`}
+        allow="autoplay"
+        allowFullScreen
+      />
+    </div>
+  );
+};
