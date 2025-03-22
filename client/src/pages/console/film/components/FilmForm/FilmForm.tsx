f3ce42ddde94baff6897712d265bc92e58117ce2
Andrii Kotliar <andrii.ktlr@outlook.com>
2025-03-22 20:28:38 +0200

    remove outdated components

--- client/src/pages/console/film/components/FilmForm/FilmForm.tsx
+++ client/src/pages/console/film/components/FilmForm/FilmForm.tsx
@@ -3,7 +3,6 @@ import {
   Button,
   FormCheckboxesGroup,
   FormTextInput,
-  FormVideoInput,
   FormSelect,
   FormImageInput,
 } from '@/ui';
@@ -11,9 +10,6 @@ import { FC } from 'react';
 import { FormRow } from '../FormRow/FormRow';
 import { useQueryClient } from '@tanstack/react-query';
 import { InitialData } from '@/types';
-import { useFormContext } from 'react-hook-form';
-import { FormValues } from '../../types';
-import { Description } from '../Description/Description';
 
 type FilmFormProps = {
   onSubmit: VoidFunction;
@@ -22,13 +18,10 @@ type FilmFormProps = {
 export const FilmForm: FC<FilmFormProps> = ({ onSubmit }) => {
   const queryClient = useQueryClient();
   const data = queryClient.getQueryData<InitialData>(['initial-data']);
-  const { watch } = useFormContext<FormValues>();
 
   if (!data) {
     return <div>Form options doesn't exist, reload the page.</div>;
   }
-
-  const titleType = watch('type');
 
   return (
     <form onSubmit={onSubmit} className={styles.form}>
@@ -47,13 +40,17 @@ export const FilmForm: FC<FilmFormProps> = ({ onSubmit }) => {
           type="radio"
         />
       </FormRow>
+      <FormTextInput
+        name="rating"
+        type="number"
+        label="Rating"
+        min="1"
+        max="3"
+      />
       <FormRow gap={40}>
         <div className={styles.posterWrapper}>
           <FormImageInput name="poster" label="Poster" />
         </div>
-        {titleType === 'FILM' && (
-          <FormVideoInput name="youtubeVideoId" label="Trailer" />
-        )}
       </FormRow>
       <FormSelect
         label="Genres"
@@ -87,16 +84,6 @@ export const FilmForm: FC<FilmFormProps> = ({ onSubmit }) => {
           min="0"
         />
       </FormRow>
-      <FormRow gap={40}>
-        <FormTextInput
-          name="rating"
-          type="number"
-          label="Rating"
-          min="1"
-          max="3"
-        />
-      </FormRow>
-      <Description />
       <FormRow align="center" gap={20}>
         <Button type="submit">Create</Button>
       </FormRow>
