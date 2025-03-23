780ce6ddca640ec75fa3a977af972ab0bcf9264f
Andrii Kotliar <andrii.ktlr@outlook.com>
2025-03-23 18:37:32 +0200

    move login page components to routes folder

--- client/src/routes/login/route.lazy.tsx
+++
@@ -1,6 +0,0 @@
-import { LoginPage } from '@/pages';
-import { createLazyFileRoute } from '@tanstack/react-router';
-
-export const Route = createLazyFileRoute('/login')({
-  component: LoginPage,
-});
--- client/src/routes/login/route.tsx
+++ client/src/routes/login/route.tsx
@@ -1,5 +1,59 @@
+import { AuthenticationApi } from '@/api';
 import { getIsAuthState } from '@/helpers';
-import { createFileRoute, redirect } from '@tanstack/react-router';
+import { HttpError, LocalStorage } from '@/services';
+import { AuthResponse, LoginPayload } from '@/types';
+import { yupResolver } from '@hookform/resolvers/yup';
+import { useMutation } from '@tanstack/react-query';
+import { createFileRoute, redirect, useNavigate } from '@tanstack/react-router';
+import { FormProvider, useForm } from 'react-hook-form';
+import { loginFormSchema } from './-validation';
+import { LoginForm, LoginLayout } from './-components';
+import { ErrorMessage } from '@/ui';
+
+const defaultLoginValues: LoginPayload = {
+  username: '',
+  password: '',
+};
+
+const LoginContainer = () => {
+  const navigate = useNavigate();
+
+  const { mutate, error, isPending } = useMutation<
+    AuthResponse,
+    HttpError,
+    LoginPayload
+  >({
+    mutationFn: AuthenticationApi.login,
+    onSuccess: (result) => {
+      if (result.userId) {
+        LocalStorage.setItem('IS_AUTHENTICATED', true);
+
+        navigate({ to: '/console/pending' });
+      }
+    },
+  });
+
+  const methods = useForm<LoginPayload>({
+    defaultValues: defaultLoginValues,
+    resolver: yupResolver(loginFormSchema),
+  });
+
+  const handleLogin = (values: LoginPayload) => {
+    mutate(values);
+  };
+
+  return (
+    <FormProvider {...methods}>
+      <LoginLayout>
+        <LoginForm
+          onSubmit={methods.handleSubmit(handleLogin)}
+          isSaving={isPending}
+        />
+      </LoginLayout>
+      <ErrorMessage error={error} />
+    </FormProvider>
+  );
+};
 
 export const Route = createFileRoute('/login')({
   beforeLoad: () => {
@@ -9,4 +63,5 @@ export const Route = createFileRoute('/login')({
       throw redirect({ to: '/console/pending' });
     }
   },
+  component: LoginContainer,
 });
