af9fc01627e47c7990ee4d0817bea5f01d311a73
Andrii Kotliar <andrii.ktlr@outlook.com>
2025-03-28 20:48:18 +0200

    add rating input

--- client/src/ui/index.ts
+++ client/src/ui/index.ts
@@ -35,3 +35,4 @@ export * from './FormCheckboxesGroup/FormCheckboxesGroup';
 export * from './VideoInput';
 export * from './TextArea';
 export * from './DataRow/DataRow';
+export * from './RatingInput';
