d0e3f11e210e91edd09481aed99c6e9e6d20dcb8
Andrii Kotliar <andrii.ktlr@outlook.com>
2025-04-18 20:22:49 +0300

    update video input

--- client/src/ui/VideoInput/VideoInput.tsx
+++ client/src/ui/VideoInput/VideoInput.tsx
@@ -1,8 +1,14 @@
-import { ComponentProps, forwardRef } from 'react';
+import {
+  ComponentProps,
+  FocusEventHandler,
+  forwardRef,
+  useEffect,
+  useState,
+} from 'react';
 import { TextInput } from '../TextInput';
 import { VideoPreview } from './components';
+import { YOUTUBE_VIDEO_DIRECT_URL } from '@/constants';
 import styles from './VideoInput.module.css';
-import { useUrlInput } from '@/hooks';
 
 export type VideoInputProps = {
   label?: string;
@@ -10,17 +16,53 @@ export type VideoInputProps = {
   externalWatchedValue?: string;
 } & Omit<ComponentProps<'input'>, 'type' | 'name'>;
 
+const INCORRECT_URL_ERROR = `URL should be ${YOUTUBE_VIDEO_DIRECT_URL}`;
+const REGEX_VALIDATION = /^https:\/\/www\.youtube\.com\/watch\?v=[\w-]+$/;
+
 export const VideoInput = forwardRef<HTMLInputElement, VideoInputProps>(
-  ({ onBlur, externalWatchedValue, ...textInputProps }, ref) => {
-    const { handleBlur, previewPath } = useUrlInput({
-      onBlur,
-      externalWatchedValue,
-    });
+  ({ onBlur, externalWatchedValue, error, ...textInputProps }, ref) => {
+    const [previewId, setPreviewId] = useState(externalWatchedValue ?? '');
+    const [internalError, setInternalError] = useState<string | null>(null);
+
+    const handleBlur: FocusEventHandler<HTMLInputElement> = (event) => {
+      setInternalError('');
+      onBlur?.(event);
+
+      const value = event.target.value.trim();
+
+      if (!value.length) {
+        return;
+      }
+
+      const isValidValue = REGEX_VALIDATION.test(value);
+
+      if (!isValidValue) {
+        setInternalError(INCORRECT_URL_ERROR);
+        return;
+      }
+
+      const urlData = new URL(value);
+
+      const videoId = urlData.searchParams.get('v');
+
+      setPreviewId(videoId!);
+    };
+
+    useEffect(() => {
+      if (typeof externalWatchedValue !== 'string' && previewId.length) {
+        setPreviewId('');
+      }
+    }, [externalWatchedValue, previewId]);
 
     return (
       <div className={styles.wrapper}>
-        <TextInput ref={ref} onBlur={handleBlur} {...textInputProps} />
-        <VideoPreview videoId={previewPath} />
+        <TextInput
+          ref={ref}
+          onBlur={handleBlur}
+          error={internalError ?? error}
+          {...textInputProps}
+        />
+        <VideoPreview videoId={previewId} />
       </div>
     );
   },
