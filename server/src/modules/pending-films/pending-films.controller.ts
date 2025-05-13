2cce42402cbbfb20796dc5d4308ea72390f0f597
Andrii Kotliar <andrii.ktlr@outlook.com>
2025-05-13 20:28:22 +0300

    define route to get pending film by id

--- server/src/modules/pending-films/pending-films.controller.ts
+++ server/src/modules/pending-films/pending-films.controller.ts
@@ -1,7 +1,8 @@
-import { router } from 'src/common';
+import { NotFoundException, router } from 'src/common';
 import {
   PendingFilmsCreateBodySchema,
   PendingFilmsDeleteParamsSchema,
+  PendingFilmsFindSchema,
   PendingFilmsQuerySchema,
   PendingFilmsUpdateBodySchema,
   PendingFilmsUpdateParamsSchema,
@@ -17,6 +18,30 @@ export const PendingFilmsController = router((app, defineRoute) => [
     },
     handler: async ({ request }) => {
       const data = await app.pendingFilmsService.getList(request.query);
+
+      return {
+        status: 'OK',
+        data,
+      };
+    },
+  }),
+  defineRoute({
+    method: 'GET',
+    url: '/:id',
+    preHandler: [app.authenticate],
+    schema: {
+      params: PendingFilmsFindSchema,
+    },
+    handler: async ({ request }) => {
+      const data = app.pendingFilmsService.getPendingFilmById(
+        request.params.id,
+      );
+
+      if (!data) {
+        throw new NotFoundException({
+          message: `Pending film ${request.params.id} not found`,
+        });
+      }
 
       return {
         status: 'OK',
