0a75f95e164bafa6c4205e0012757a75395ed848
Andrii Kotliar <andrii.ktlr@outlook.com>
2025-06-03 15:11:38 +0300

    fix crew select add person form

--- client/src/routes/console/manage_/-components/film-form/components/crew-select/crew-select.tsx
+++ client/src/routes/console/manage_/-components/film-form/components/crew-select/crew-select.tsx
@@ -1,6 +1,6 @@
 import { searchPersonQuery } from '@/queries';
 import { FormValues } from '@/routes/console/manage_/-types';
-import { ListOption } from '@/types';
+import { ListOption, Person } from '@/types';
 import {
   Button,
   FormSection,
@@ -11,9 +11,13 @@ import {
 } from '@/components';
 import { Trash2Icon } from 'lucide-react';
 import { FC, useState } from 'react';
-import { useFieldArray, useFormContext } from 'react-hook-form';
+import { FormProvider, useFieldArray, useFormContext } from 'react-hook-form';
 import styles from './crew-select.module.css';
-import { CreatePersonModal } from '@/routes/console/-components';
+import { FormModal, PersonForm } from '@/routes/console/-components';
+import {
+  useManagePerson,
+  usePersonForm,
+} from '@/routes/console/-components/person-form/hooks';
 
 type CrewSelectProps = {
   positionOptions: ListOption[];
@@ -23,9 +27,27 @@ export const CrewSelect: FC<CrewSelectProps> = ({ positionOptions }) => {
   const { control } = useFormContext<FormValues>();
   const [isCreateModalOpen, setIsCreateModalOpen] = useState(false);
 
+  const form = usePersonForm();
+
   const { fields, append, remove } = useFieldArray({
     control,
     name: 'crew',
+  });
+
+  const handleAddNewPerson = (data: Person) => {
+    append({
+      personId: data.id,
+      name: data.name,
+      comment: null,
+      position: '',
+    });
+  };
+
+  const { mutate: createPerson } = useManagePerson({
+    onSuccessHandler: (data) => {
+      handleAddNewPerson(data);
+      setIsCreateModalOpen(false);
+    },
   });
 
   return (
@@ -73,19 +95,17 @@ export const CrewSelect: FC<CrewSelectProps> = ({ positionOptions }) => {
           </div>
         ))}
       </div>
-      <CreatePersonModal
+      <FormModal
         isOpen={isCreateModalOpen}
         onClose={() => setIsCreateModalOpen(false)}
-        onCreate={(data) => {
-          append({
-            personId: data.id,
-            name: data.name,
-            position: '',
-            comment: null,
-          });
-        }}
-        title="Add crew member"
-      />
+      >
+        <FormProvider {...form}>
+          <PersonForm
+            onSubmit={form.handleSubmit((values) => createPerson(values))}
+            title="Add crew member"
+          />
+        </FormProvider>
+      </FormModal>
     </FormSection>
   );
 };
