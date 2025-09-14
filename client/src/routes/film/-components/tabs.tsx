3944ba73aeaf6c2fc5522eb717efa1685f171c00
Andrii Kotliar <andrii.ktlr@outlook.com>
2025-09-14 20:45:56 +0300

    move active page regex to a variable

--- client/src/routes/film/-components/tabs.tsx
+++ client/src/routes/film/-components/tabs.tsx
@@ -7,6 +7,8 @@ type TabsProps = {
   film: FilmDetails;
 };
 
+const NUMBERS_SEQUENCE_REGEX = /\d+/;
+
 export const Tabs = ({ film }: TabsProps) => {
   const location = useRouterState({ select: (s) => s.location });
   const config = filmTabsConfig.filter((item) => film[item.condition]);
@@ -19,7 +21,7 @@ export const Tabs = ({ film }: TabsProps) => {
           key={item.route}
           className={classNames('pb-1 hover:text-sky-700 hover:border-b-sky-700', {
             'text-sky-700 border-b-2 border-b-sky-700 pointer-events-none font-medium':
-              location.pathname.replace(/\d+/, '$id') === item.route,
+              location.pathname.replace(NUMBERS_SEQUENCE_REGEX, '$id') === item.route,
           })}
         >
           {item.title}
