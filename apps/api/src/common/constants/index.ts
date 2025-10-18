ea5b0f010d6836787d119d127c90dd04934767c2
Andrii Kotliar <andrii.ktlr@outlook.com>
2025-10-18 17:35:08 +0300

    api: reduce token lifetime

--- apps/api/src/common/constants/index.ts
+++ apps/api/src/common/constants/index.ts
@@ -1,6 +1,6 @@
 export const DEFAULT_PAGINATION_LIMIT = 30;
 export const DEFAULT_SEARCH_LIMIT = 10;
-export const ACCESS_TOKEN_MAX_AGE_SEC = 86_400; // 24 hours
+export const ACCESS_TOKEN_MAX_AGE_SEC = 900; // 15 minutes
 export const REFRESH_TOKEN_MAX_AGE_SEC = 604_800; // 7 days
 export const TIME_1_MIN = 60_000;
 export const NEW_ITEM_ID = 'new';
