0caf4ba36384cf764156b204e4a8f6436aeb8267
Andrii Kotliar <andrii.ktlr@outlook.com>
2025-12-02 19:39:02 +0200

    fix types

--- apps/web/src/routes/console/-shared/components/add-item-button/add-item-button.tsx
+++ apps/web/src/routes/console/-shared/components/add-item-button/add-item-button.tsx
@@ -1,9 +1,10 @@
 import { Button, type ButtonProps } from '~/shared';
 import { PlusIcon } from 'lucide-react';
+import type { PropsWithChildren } from 'react';
 
 type AddItemButtonProps = Omit<ButtonProps, 'variant' | 'icon'>;
 
-export const AddItemButton = (props: AddItemButtonProps) => {
+export const AddItemButton = (props: PropsWithChildren<AddItemButtonProps>) => {
   return (
     <div>
       <Button variant="light" icon={<PlusIcon />} {...props} />
