2997fb763ec9e3149fb728ef17c85fe00851ede3
Andrii Kotliar <andrii.ktlr@gmail.com>
2025-12-13 19:15:37 +0200

    fix import

--- apps/api/src/routers/index.ts
+++ apps/api/src/routers/index.ts
@@ -1,18 +1,18 @@
 import type { Route } from '~/shared';
-import authRouter from '~/routes/auth.router';
-import awardsRouter from '~/routes/awards.router';
-import chapterKeysRouter from '~/routes/chapter-keys.router';
-import collectionEventsRouter from '~/routes/collection-events.router';
-import collectionsRouter from '~/routes/collections.router';
-import countriesRouter from '~/routes/countries.router';
-import filesRouter from '~/routes/files.router';
-import filmsRouter from '~/routes/films.router';
-import genresRouter from '~/routes/genres.router';
-import initialDataRouter from '~/routes/initial-data.router';
-import pageContentRouter from '~/routes/page-content.router';
-import pendingFilmsRouter from '~/routes/pending-films.router';
-import peopleRouter from '~/routes/people.router';
-import studiosRouter from '~/routes/studios.router';
+import authRouter from '~/routers/auth.router';
+import awardsRouter from '~/routers/awards.router';
+import chapterKeysRouter from '~/routers/chapter-keys.router';
+import collectionEventsRouter from '~/routers/collection-events.router';
+import collectionsRouter from '~/routers/collections.router';
+import countriesRouter from '~/routers/countries.router';
+import filesRouter from '~/routers/files.router';
+import filmsRouter from '~/routers/films.router';
+import genresRouter from '~/routers/genres.router';
+import initialDataRouter from '~/routers/initial-data.router';
+import pageContentRouter from '~/routers/page-content.router';
+import pendingFilmsRouter from '~/routers/pending-films.router';
+import peopleRouter from '~/routers/people.router';
+import studiosRouter from '~/routers/studios.router';
 
 export const routers: Record<string, Route[]> = {
   auth: authRouter,
