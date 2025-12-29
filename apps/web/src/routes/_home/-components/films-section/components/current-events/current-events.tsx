557be0b758959b5bee5a4d9a9a1f2ba09ca86f63
Andrii Kotliar <andrii.ktlr@gmail.com>
2025-12-29 17:13:09 +0200

    web: update events row design

--- apps/web/src/routes/_home/-components/films-section/components/current-events/current-events.tsx
+++ apps/web/src/routes/_home/-components/films-section/components/current-events/current-events.tsx
@@ -1,21 +1,7 @@
-import styles from "./current-events.module.css";
-import { BREAKPOINTS, fetchInitialDataQuery } from '~/shared';
+import styles from './current-events.module.css';
+import { fetchInitialDataQuery } from '~/shared';
 import { useQuery } from '@tanstack/react-query';
 import { EventBanner } from '~/routes/_home/-components/films-section/components/event-banner/event-banner';
-
-const getPlaceholdersCount = (eventsCount: number) => {
-  const screenWidth = window.innerWidth;
-
-  if (screenWidth >= BREAKPOINTS.xl2) {
-    return 4 - eventsCount;
-  }
-
-  if (screenWidth >= BREAKPOINTS.xl) {
-    return eventsCount % 2;
-  }
-
-  return 0;
-};
 
 export const CurrentEvents = () => {
   const { data: initialData } = useQuery(fetchInitialDataQuery());
@@ -24,17 +10,11 @@ export const CurrentEvents = () => {
     return null;
   }
 
-  const placeholdersCount = getPlaceholdersCount(initialData.events.length);
-
   return (
-    <div className={styles.events_grid}>
+    <div className={styles.events_track}>
       {initialData.events.map((event) => (
         <EventBanner event={event} key={event.id} />
       ))}
-      {placeholdersCount > 0 &&
-        Array.from({ length: placeholdersCount }, (_, index) => (
-          <div className={styles.event_placeholder} key={index} />
-        ))}
     </div>
   );
 };
--- apps/web/src/routes/_home/-components/films-section/components/event-banner/event-banner.tsx
+++ apps/web/src/routes/_home/-components/films-section/components/event-banner/event-banner.tsx
@@ -1,43 +1,23 @@
-import styles from './event-banner.module.css';
-import { getDateMonthLabel, type CollectionEventFilled } from '~/shared';
-import { Image } from '~/shared/components/image/image';
 import { Link } from '@tanstack/react-router';
-import { ArrowRightIcon, CalendarIcon, TimerIcon } from 'lucide-react';
-import { EventBannerDataRow } from '~/routes/_home/-components/films-section/components/event-banner/components';
+import styles from './event-banner.module.css';
+import { type CollectionEventFilled } from '~/shared';
+import { Image } from '~/shared/components/image/image';
 
 type EventBannerProps = {
   event: CollectionEventFilled;
 };
 
 export const EventBanner = ({ event }: EventBannerProps) => {
-  const currentYear = new Date().getFullYear();
-
   return (
     <Link
       to="/"
       search={{ collectionId: event.collection.id.toString() }}
-      className={styles.event_banner_link}
+      className={styles.event_banner_button}
     >
-      <div className={styles.poster_wrapper}>
+      <div className={styles.event_banner_inner}>
         <Image src={event.film.poster} isExternal className={styles.poster_image} />
-        <div className={styles.poster_overlay} />
       </div>
-      <div className={styles.data}>
-        <div className={styles.data_title}>
-          <span>Ongoing event</span>
-        </div>
-        <div className={styles.event_title}>{event.title}</div>
-        <div className={styles.event_data}>
-          <EventBannerDataRow icon={<CalendarIcon size={16} />} value={getDateMonthLabel(event)} />
-          <EventBannerDataRow
-            icon={<TimerIcon size={16} />}
-            value={event.yearFrom ? `${currentYear - event.yearFrom} years` : 'Regular event'}
-          />
-        </div>
-      </div>
-      <div className={styles.link_icon}>
-        <ArrowRightIcon size={14} />
-      </div>
+      <div className={styles.event_title}>{event.title}</div>
     </Link>
   );
 };
