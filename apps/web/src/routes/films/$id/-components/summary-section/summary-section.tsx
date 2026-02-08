a75ac4998672c190bd8ac0a9401780a499b10d6b
Andrii Kotliar <andrii.ktlr@gmail.com>
2026-02-08 21:09:04 +0200

    fix no poster layout

--- apps/web/src/routes/films/$id/-components/summary-section/summary-section.tsx
+++ apps/web/src/routes/films/$id/-components/summary-section/summary-section.tsx
@@ -9,6 +9,7 @@ import {
 } from '~/routes/films/$id/-components/summary-section/components';
 import { getFilmSummaryConfig } from '~/routes/films/$id/-helpers';
 import { Title } from '~/routes/films/$id/-components/summary-section/components/title/title';
+import clsx from 'clsx';
 
 type SummarySectionProps = {
   film: ApiResponse<typeof api.films.get>;
@@ -31,7 +32,7 @@ export const SummarySection = ({ film }: SummarySectionProps) => {
         <Rating value={film.rating} />
       </div>
 
-      <div className={styles.content}>
+      <div className={clsx(styles.content, !film.poster && styles.content_no_poster)}>
         <div className={styles.left_column}>
           {film.poster && <Poster image={film.poster} title={film.title} />}
           {film.trailers.length > 0 && <TrailersButton data={film.trailers} type={film.type} />}
