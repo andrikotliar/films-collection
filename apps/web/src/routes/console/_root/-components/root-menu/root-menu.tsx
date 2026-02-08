ba9d224ae7ad6af092896bc6865ab5e7b5b0da3d
Andrii Kotliar <andrii.ktlr@gmail.com>
2026-02-08 18:32:31 +0200

    update console root page design

--- apps/web/src/routes/console/_root/-components/root-menu/root-menu.tsx
+++ apps/web/src/routes/console/_root/-components/root-menu/root-menu.tsx
@@ -1,25 +1,23 @@
 import styles from './root-menu.module.css';
 import { Link } from '@tanstack/react-router';
-import { consoleMenuConfig, defineCssProperties, Panel } from '~/shared';
+import { consoleMenuConfig, defineCssProperties } from '~/shared';
 
 export const RootMenu = () => {
   return (
-    <Panel>
-      <div className={styles.root_menu}>
-        {consoleMenuConfig.map((item) => (
-          <Link
-            key={item.id}
-            to={item.route}
-            className={styles.root_menu_item}
-            style={defineCssProperties({
-              '--console-link-color': `var(--${item.color})`,
-            })}
-          >
-            <div className={styles.root_menu_icon}>{item.icon}</div>
-            {item.title}
-          </Link>
-        ))}
-      </div>
-    </Panel>
+    <div className={styles.root_menu}>
+      {consoleMenuConfig.map((item) => (
+        <Link
+          key={item.id}
+          to={item.route}
+          className={styles.root_menu_item}
+          style={defineCssProperties({
+            '--console-link-color': `var(--${item.color})`,
+          })}
+        >
+          <div className={styles.root_menu_icon}>{item.icon}</div>
+          {item.title}
+        </Link>
+      ))}
+    </div>
   );
 };
