2a62fb0ae18ee0f1ae779a071bc20337c54c5623
Andrii Kotliar <andrii.ktlr@gmail.com>
2026-02-20 19:25:53 +0200

    migrate chapter keys

--- apps/api/src/services/chapter-keys/chapter-keys.repository.ts
+++ apps/api/src/services/chapter-keys/chapter-keys.repository.ts
@@ -1,21 +1,14 @@
-import type { Deps } from '~/shared';
+import { filmChapterKeys } from '~/database/schema';
+import { getFirstValue, type Deps } from '~/shared';
 
 export class ChapterKeysRepository {
-  constructor(private readonly deps: Deps<'databaseService'>) {}
+  constructor(private readonly deps: Deps<'db'>) {}
 
   getAll() {
-    return this.deps.databaseService.filmChapterKey.findMany({
-      select: {
-        key: true,
-      },
-    });
+    return this.deps.db.select({ key: filmChapterKeys.key }).from(filmChapterKeys);
   }
 
-  create(key: string) {
-    return this.deps.databaseService.filmChapterKey.create({
-      data: {
-        key,
-      },
-    });
+  async create(key: string) {
+    return getFirstValue(await this.deps.db.insert(filmChapterKeys).values({ key }).returning());
   }
 }
