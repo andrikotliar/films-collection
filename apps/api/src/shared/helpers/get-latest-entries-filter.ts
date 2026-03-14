cabacacb192b1ecb584e95293cddaeb6aafe2053
Andrii Kotliar <andrii.ktlr@gmail.com>
2026-03-14 16:39:35 +0200

    fix raw query

--- apps/api/src/shared/helpers/get-latest-entries-filter.ts
+++ apps/api/src/shared/helpers/get-latest-entries-filter.ts
@@ -2,5 +2,5 @@ import { gte, sql } from 'drizzle-orm';
 import type { PgColumn } from 'drizzle-orm/pg-core';
 
 export const getLatestEntriesFilter = (column: PgColumn, days: number = 7) => {
-  return gte(column, sql`NOW() - INTERVAL '${days} days'`);
+  return gte(column, sql.raw(`NOW() - INTERVAL '${days} days'`));
 };
