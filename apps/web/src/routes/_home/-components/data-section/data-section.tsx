50cf36c86fc642c7651ef226987a5d7e64482201
Andrii Kotliar <andrii.ktlr@gmail.com>
2026-04-18 22:08:35 +0300

    add new item button to the data section component

--- apps/web/src/routes/_home/-components/data-section/data-section.tsx
+++ apps/web/src/routes/_home/-components/data-section/data-section.tsx
@@ -1,16 +1,28 @@
-import { Loader } from '~/shared';
+import { Button, Loader } from '~/shared';
 import styles from './data-section.module.css';
+import { PlusIcon } from 'lucide-react';
 
 type DataSectionProps = {
   title: string;
   children?: React.ReactNode;
   isLoading?: boolean;
+  onAddItem?: () => void;
 };
 
-export const DataSection = ({ title, children, isLoading = false }: DataSectionProps) => {
+export const DataSection = ({
+  title,
+  children,
+  isLoading = false,
+  onAddItem,
+}: DataSectionProps) => {
   return (
     <div className={styles.wrapper}>
-      <div className={styles.title}>{title}</div>
+      <div className={styles.header}>
+        <div className={styles.title}>{title}</div>
+        {typeof onAddItem === 'function' && (
+          <Button onClick={onAddItem} icon={<PlusIcon />} size="small" variant="ghost" />
+        )}
+      </div>
       <div className={styles.content}>
         {children}
         {isLoading && (
