d9fe15f9b7756525a9c34b3ae44cd3ee316eb2d7
Andrii Kotliar <andrii.ktlr@gmail.com>
2026-05-02 21:31:59 +0300

    remove dashboard page

--- apps/web/src/routes/_home/-components/layout/layout.tsx
+++
@@ -1,9 +0,0 @@
-import styles from './layout.module.css';
-
-type LayoutProps = {
-  children?: React.ReactNode;
-};
-
-export const Layout = ({ children }: LayoutProps) => {
-  return <div className={styles.wrapper}>{children}</div>;
-};
