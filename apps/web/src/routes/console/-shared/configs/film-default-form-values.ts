186d5991cc8d2b82db7953bb82966343d7a74a61
Andrii Kotliar <andrii.ktlr@gmail.com>
2026-05-10 17:11:03 +0300

    fix import

--- apps/web/src/routes/console/-shared/configs/film-default-form-values.ts
+++ apps/web/src/routes/console/-shared/configs/film-default-form-values.ts
@@ -1,6 +1,6 @@
 import type z from 'zod';
 import { NEW_ITEM_ID } from '@films-collection/shared';
-import type { FilmFormSchema } from '~/routes/console/-shared/schemas';
+import type { FilmFormSchema } from '~/routes/console/films_/-components/film-form/-schemas';
 import { getTodayString } from '~/shared';
 
 export const filmDefaultFormValues: z.infer<typeof FilmFormSchema> = {
