ecc08176ea4adb5d6267b1ab0b7617d7aacfd72f
Andrii Kotliar <andrii.ktlr@gmail.com>
2026-05-23 18:17:15 +0300

    extend styles enum

--- packages/shared/src/enums/title-style.enum.ts
+++ packages/shared/src/enums/title-style.enum.ts
@@ -3,6 +3,7 @@ import type { Enum } from '~/types/enum.type.js';
 export const TitleStyle = {
   LIVE_ACTION: 'LIVE_ACTION',
   ANIMATION: 'ANIMATION',
+  ANIME: 'ANIME',
 } as const;
 
 export type TTitleStyle = Enum<typeof TitleStyle>;
