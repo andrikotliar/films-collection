aab379fea4fd42c7e23419bfbcb6e8cbbf9ebaa1
Andrii Kotliar <andrii.ktlr@gmail.com>
2026-05-31 13:30:59 +0300

    remove anime style

--- packages/shared/src/enums/title-style.enum.ts
+++ packages/shared/src/enums/title-style.enum.ts
@@ -3,7 +3,6 @@ import type { Enum } from '~/types/enum.type.js';
 export const TitleStyle = {
   LIVE_ACTION: 'LIVE_ACTION',
   ANIMATION: 'ANIMATION',
-  ANIME: 'ANIME',
 } as const;
 
 export type TTitleStyle = Enum<typeof TitleStyle>;
