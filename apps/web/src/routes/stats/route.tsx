ef333dafea0fd1b172ec89337a05997bebc43f39
Andrii Kotliar <andrii.ktlr@gmail.com>
2026-08-09 18:29:23 +0300

    display charts on the route page

--- apps/web/src/routes/stats/route.tsx
+++ apps/web/src/routes/stats/route.tsx
@@ -1,6 +1,8 @@
+import { useSuspenseQuery } from '@tanstack/react-query';
 import { createFileRoute } from '@tanstack/react-router';
 import { StatsLayout } from '~/routes/stats/-components/stats-layout/stats-layout';
 import { getFilmsStatsQueryOptions, PageTitle } from '~/shared';
+import { Chart } from '~/shared/components/chart/chart';
 
 export const Route = createFileRoute('/stats')({
   component: RouteComponent,
@@ -13,9 +15,20 @@ export const Route = createFileRoute('/stats')({
 });
 
 function RouteComponent() {
+  const { data } = useSuspenseQuery(getFilmsStatsQueryOptions());
+
   return (
     <StatsLayout>
       <PageTitle>Statistic</PageTitle>
+      {data.map((chart) => (
+        <Chart
+          key={chart.block}
+          label={chart.block}
+          xAxisMaxValue={227}
+          xAxisStep={20}
+          data={chart.stats}
+        />
+      ))}
     </StatsLayout>
   );
 }
