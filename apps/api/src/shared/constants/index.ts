0ad4de15f3fe0f9fe1b15ac0ad654c57a6ab53f8
Andrii Kotliar <andrii.ktlr@gmail.com>
2026-08-09 16:38:00 +0300

    increase refresh token time

--- apps/api/src/shared/constants/index.ts
+++ apps/api/src/shared/constants/index.ts
@@ -1,4 +1,4 @@
 export const ACCESS_TOKEN_MAX_AGE_SEC = 900; // 15 minutes
-export const REFRESH_TOKEN_MAX_AGE_SEC = 604_800; // 7 days
+export const REFRESH_TOKEN_MAX_AGE_SEC = 2_592_000; // 7 days
 export const TIME_1_MIN = 60_000;
 export const HASH_SALT_ROUNDS = 10;
