a59b03414437bd86798b96d192b0e39499207b88
Andrii Kotliar <andrii.ktlr@gmail.com>
2026-08-24 22:45:25 +0300

    fix di container proxy type

--- apps/api/src/plugins/services.plugin.ts
+++ apps/api/src/plugins/services.plugin.ts
@@ -2,16 +2,14 @@ import type { FastifyInstance } from 'fastify';
 import fastifyPlugin from 'fastify-plugin';
 import { services, type ApiServices } from '~/modules/app.module.js';
 import { DiContainer } from '~/shared/services/di-container.js';
+import type { ServiceInstances } from '~/shared/types/deps.js';
 
 export const diContainerDecorator = async (app: FastifyInstance) => {
   const container = new DiContainer({ ...services, Database: app.db, Jwt: app.jwt });
 
-  const servicesProxy = new Proxy(
-    {} as { [K in keyof ApiServices]: InstanceType<ApiServices[K]> },
-    {
-      get: (_, key: keyof ApiServices) => container.resolve(key),
-    },
-  );
+  const servicesProxy = new Proxy({} as ServiceInstances, {
+    get: (_, key: keyof ApiServices) => container.resolve(key),
+  });
 
   app.decorate('services', servicesProxy);
 };
