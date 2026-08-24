b668585f0f152d5aa9a88cffeec5774a4cceeb2b
Andrii Kotliar <andrii.ktlr@gmail.com>
2026-08-24 22:46:18 +0300

    remove module leftover

--- apps/api/src/modules/ai/ai.module.ts
+++
@@ -1,6 +0,0 @@
-import { AiService } from '~/modules/ai/ai.service.js';
-import { createApiModule } from '~/shared/helpers/create-api-module.js';
-
-export const AiModule = createApiModule({
-  services: { AiService },
-});
