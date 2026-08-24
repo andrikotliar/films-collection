c26ccb8b825d3ba50de8adc3282f1e247a6cbb02
Andrii Kotliar <andrii.ktlr@gmail.com>
2026-08-24 21:13:59 +0300

    use service instances directly in routes

--- apps/api/src/shared/helpers/create-router.ts
+++ apps/api/src/shared/helpers/create-router.ts
@@ -1,7 +1,8 @@
 import type { ApiContract, ContractDefinition, RouteSchema } from '@films-collection/api-client';
-import type { FastifyInstance, FastifyReply, FastifyRequest } from 'fastify';
+import type { FastifyReply, FastifyRequest } from 'fastify';
 import type z from 'zod';
 import type { ResponseStatus } from '~/shared/enums/response-code.js';
+import type { ServiceInstances } from '~/shared/types/dependencies.js';
 
 type InferZod<S, R = undefined> = S extends z.ZodType ? z.infer<S> : R;
 
@@ -16,7 +17,7 @@ type InferResponse<S extends RouteSchema> = InferZod<S['response'], unknown>;
 type HandlerContext<S extends RouteSchema> = {
   request: FastifyRequest<InferRequest<S>>;
   reply: FastifyReply;
-  app: FastifyInstance;
+  services: ServiceInstances;
 };
 
 type PreHandler = (request: FastifyRequest, reply: FastifyReply) => void;
